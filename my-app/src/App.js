a8279c3163e1625236adaf93e77414bdffcfcf2f
mirielimacedo <mirielimacedo@gmail.com>
2024-04-30 16:12:22 +0800

    Tester package.json

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src="https://www.shecodes.io/assets/branding/logo-shecodes-3dfa60aeab8ef361842da5a2b6d46db3af1b7afafefee3dde0a9846389de754b.png"
+          className="App-logo img-fluid"
+          alt="logo"
+        ></img>
       </header>
     </div>
   );
 }
-
-export default App;
